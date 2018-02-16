91131ae201d7cd4dd7659a3f1c632cc0d9898a41
Prasant Adhikari <pa1038@ad.nyu.edu>
2018-02-16 15:15:38 +0400

    Added a round counter process
    state_machine_process stays sets and holds round_number at 0 when clr is set. An en bit is added to prevent moving to next state on the next rising edge if the clr bit is unset at the middle of a clock cycle. This way, state 0 gets a full clock cycle to run.
    
    The state machine first counts up from 0 to 10. It wraps around 0 if enc
    is set (for encryption) or moves to 10, counts down to 0 and cycles if
    enc is unset.

--- aes_module.vhd
+++ aes_module.vhd
@@ -31,15 +31,61 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity aes_module is
     Port ( clr : in  STD_LOGIC;
+			  clk : in  STD_LOGIC;
            enc : in  STD_LOGIC;
            key : in  STD_LOGIC_VECTOR (128 downto 0);
-           d : in  STD_LOGIC_VECTOR (128 downto 0);
-           q : out  STD_LOGIC_VECTOR (0 downto 0));
+           d   : in  STD_LOGIC_VECTOR (128 downto 0);
+           q   : out STD_LOGIC_VECTOR (128 downto 0));
 end aes_module;
 
 architecture Behavioral of aes_module is
 
+	-- the signal to keep track of the current round number
+	signal round_number : integer range 0 to 10 := 0;
+	
+	-- the bit to persist state 0 for a full cycle
+	signal enable_module : bit := '0';
+	signal keys_filled   : bit := '0';  -- the bit to indicate that all keys have been filled
 begin
-				
+
+state_machine_process:
+process(clr, clk)
+begin
+	if clr = '1' then
+		round_number <= 0;
+		keys_filled <= '0';
+	elsif rising_edge(clk) then
+		if en = '0' then
+			en <= '1';
+		elsif keys_filled = '0' then
+			if round_number = 10 then
+				keys_filled <= '1';
+				if enc = '1' then
+					round_number <= 0;
+				else
+					round_number <= 10;
+				end if;
+			else
+				round_number <= round_number + 1;
+			end if;
+		else
+			if enc = '1' then
+				if round_number = 10 then
+					round_number <= 0;
+				else
+					round_number <= round_number + 1;
+				end if;
+			else
+				if round_number = 0 then
+					round_number <= 10;
+				else
+					round_number <= round_number - 1;
+				end if;
+			end if;
+		end if;
+	end if;
+end process;
+
+	
 end Behavioral;
 
